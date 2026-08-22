af4fe05147dac2454e2d1e32dbad42e0b2cfca52
ffbe34 <gustaf.bergqvist@folksam.se>
2026-08-22 09:24:25 +0200

    nvim: remember Neo-tree width

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,24 @@
+local explorer_width = 30
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
+      window = {
+        width = function()
+          return explorer_width
+        end,
+      },
+      event_handlers = {
+        {
+          event = "neo_tree_window_before_close",
+          handler = function(args)
+            if vim.api.nvim_win_is_valid(args.winid) then
+              explorer_width = vim.api.nvim_win_get_width(args.winid)
+            end
+          end,
+        },
+      },
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
